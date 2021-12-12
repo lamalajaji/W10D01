6a9630a5b067dd92324c8baf6172986bf8ea097c
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-12 14:42:27 +0300

    fetch data

---
+++ next-app/pages/Home/index.js
@@ -0,0 +1,31 @@
+
+function Home({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default Home
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+
+
+   
+
--- next-app/pages/components/navbar.js
+++ next-app/pages/components/navbar.js
@@ -1,11 +1,11 @@
-import Link from "next/link"
+import Link from "next/link";
 function NavBar() {
   return(
   <div>
     <ul> 
      
 
-   <li>  <Link href="/">  Home </Link> </li>
+   <li>  <Link href="/Home">  Home </Link> </li>
    <li><Link href="/About"> About  </Link></li>
    <li> <Link href="/Contact"> Contact  </Link></li>
     </ul>
